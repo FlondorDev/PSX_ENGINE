31d75a359e31b1721d346ccf7a36b9beb2f5829e
cons.domenico.landri <cons.domenico.landricina@cy4gate.com>
2022-08-03 17:51:24 +0200

    removed c version and added 3d cube

--- CPP/graph.cpp
+++ CPP/graph.cpp
@@ -1,18 +1,20 @@
 #include "graph.h"
+#include <strings.h>
+#include "cube.c"
 
-void DisplayBuffer::InitBuffer(bool isDB, bool isSecondDB){
-    if(isDB){
+void DisplayBuffer::InitBuffer(bool isDoubleB, bool isSecondDB){
+    if(isDoubleB){
         if(!isSecondDB){
-            SetDefDispEnv(&disp, 0, 0, 320, 240);   //set Display env
-            SetDefDrawEnv(&draw, 0, 240, 320, 240); //set Draw env
+            SetDefDispEnv(&disp, 0, 0, SCREENXRES, SCREENYRES);   //set Display env
+            SetDefDrawEnv(&draw, 0, SCREENYRES, SCREENXRES, SCREENYRES); //set Draw env
         }else{
-            SetDefDispEnv(&disp, 0, 240, 320, 240);   //set Display env
-            SetDefDrawEnv(&draw, 0, 0, 320, 240); //set Draw env    
+            SetDefDispEnv(&disp, 0, SCREENYRES, SCREENXRES, SCREENYRES);   //set Display env
+            SetDefDrawEnv(&draw, 0, 0, SCREENXRES, SCREENYRES); //set Draw env    
         }
     }else{
         ResetGraph(0);
-        SetDefDispEnv(&disp, 0, 0, 320, 240);   //set Display env
-        SetDefDrawEnv(&draw, 0, 0, 320, 240); //set Draw env
+        SetDefDispEnv(&disp, 0, 0, SCREENXRES, SCREENYRES);   //set Display env
+        SetDefDrawEnv(&draw, 0, 0, SCREENXRES, SCREENYRES); //set Draw env
     }
     draw.isbg = 1;               // Enable clear
     setRGB0(&draw, 63, 0, 127);  // Set clear color (dark purple)
@@ -29,20 +31,20 @@ void DisplayBuffer::Render() {
     PutDispEnv(&disp);      // Apply the DISPENV/DRAWENVs
     PutDrawEnv(&draw);
 
-    SetDispMask(1);             // Enable the display
-
     DrawOTag(ot+OTLEN-1);   // Draw the ordering table
     
     nextpri = pribuff;      // Reset next primitive pointer
-    ClearOTagR(ot, OTLEN);  // Clear ordering table
-    
 }
 
 void DoubleDB::InitGraph(){
     // Reset graphics
     ResetGraph(0);
+    InitGeom();
+    SetGeomOffset(CENTERX, CENTERY);        // x, y offset
+    SetGeomScreen(CENTERX);                 // Distance between eye and screen  
     DB[0].InitBuffer(true);
     DB[1].InitBuffer(true,true);
+    SetDispMask(1);             // Enable the display
 }
 
 int DoubleDB::db_index = 0;
@@ -61,8 +63,70 @@ void CreateTile(int x, int y, int w, int h, u_char r, u_char g, u_char b, Displa
     setXY0((TILE*)nextpri, x, y);       // Set primitive (x,y) position
     setWH((TILE*)nextpri, w, h);        // Set primitive size
     setRGB0((TILE*)nextpri, r, g, b); // Set color yellow
-    addPrim(db->ot, (TILE*)nextpri);      // Add primitive to the ordering table
+    addPrim(&db->ot, (TILE*)nextpri);      // Add primitive to the ordering table
     
-    nextpri += sizeof(TILE);    // Advance the next primitive pointer
+    nextpri += sizeof(TILE);   // Advance the next primitive pointer
 }
+
+void CreateTriangle(int x, int y, int half_w, int half_h, u_char r, u_char g, u_char b, DisplayBuffer* db){
+    // char const& triangle_size = 2;
+    // char const& half_size = 1;
+    // CreateTile(x - half_size, y - half_size, triangle_size, triangle_size, 255, 0, 0, db); // center
+
+    // CreateTile(x - half_size, y - half_size - half_h, triangle_size, triangle_size, 255, 0, 0, db); // top
+    // CreateTile(x - half_size - half_w, y - half_size + half_h, triangle_size, triangle_size, 255, 0, 0, db); // bottom_left
+    // CreateTile(x - half_size + half_w, y - half_size + half_h, triangle_size, triangle_size, 255, 0, 0, db); // bottom_right
+
+    char* &nextpri = db->nextpri;
+
+    setPolyF3((POLY_F3*)nextpri);              // Initialize the primitive (very important)
+    setXY3((POLY_F3*)nextpri, x- half_w, y - half_h, x - half_w, y + half_h, x + half_w, y + half_h);       // Set primitive (x,y) position
+    setRGB0((POLY_F3*)nextpri, r, g, b); // Set color yellow
+    addPrim(db->ot, (POLY_F3*)nextpri);      // Add primitive to the ordering table
+    
+    nextpri += sizeof(POLY_F3);   // Advance the next primitive pointer
+}
+
+void LoadDebugFont(){
+    // Load the internal font texture
+    FntLoad(960, 0);
+}
+
+void PrintText(char* str, int x, int y){
+     // Create the text stream
+    FntOpen(x, y, 0, 0, 0, strlen(str));
+    FntPrint(str);
+    FntFlush(-1);
+}
+
+void CreateCube(DisplayBuffer* db, SVECTOR* Rotate, VECTOR*  Trans, VECTOR*  Scale, MATRIX*  Matrix){
+    long t, p, OTz, Flag;                // t == vertex count, p == depth cueing interpolation value, OTz ==  value to create Z-ordered OT, Flag == see LibOver47.pdf, p.143
+
+    RotMatrix(Rotate, Matrix);
+    TransMatrix(Matrix, Trans);
+    ScaleMatrix(Matrix, Scale);
+    SetRotMatrix(Matrix);
+    SetTransMatrix(Matrix);
+    POLY_G3* poly = {0}; 
+    t = 0;
+    for (u_int i = 0; i < (modelCube.len*3); i += 3) {      
+        poly = (POLY_G3 *)db->nextpri;
+        // Initialize the primitive and set its color values
+        setPolyG3(poly);
+        setRGB0(poly, modelCube.c[i].r , modelCube.c[i].g   , modelCube.c[i].b);
+        setRGB1(poly, modelCube.c[i+2].r, modelCube.c[i+2].g, modelCube.c[i+2].b);
+        setRGB2(poly, modelCube.c[i+1].r, modelCube.c[i+1].g, modelCube.c[i+1].b);
+        // Rotate, translate, and project the vectors and output the results into a primitive
+        OTz  = RotTransPers(&modelCube_mesh[modelCube_index[t]]  , (long*)&poly->x0, &p, &Flag);
+        OTz += RotTransPers(&modelCube_mesh[modelCube_index[t+2]], (long*)&poly->x1, &p, &Flag);
+        OTz += RotTransPers(&modelCube_mesh[modelCube_index[t+1]], (long*)&poly->x2, &p, &Flag);
+        // Sort the primitive into the OT
+        OTz /= 3;
+        if ((OTz > 0) && (OTz < OTLEN)) addPrim(&db->ot[OTz-2], poly);
+
+        db->nextpri += sizeof(POLY_G3);
+        t+=3;
+    }
+}
+
 
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -8,28 +8,39 @@
 int main() {
     int x = 0;
     int y = 0;
-    int speed = 3;
+    int speed = 80;
     COLOR *col = new COLOR;
     col->r = 240;
     col->b = 80;
     col->g = 0;
 
+    SVECTOR Rotate={ 232, 232, 0, 0 };                   // Rotation coordinates
+    VECTOR  Trans={ 0, 0, CENTERX * 3, 0 };     // Translation coordinates
+                                            // Scaling coordinates
+    VECTOR  Scale={ ONE, ONE/2, ONE, 0 };     // ONE == 4096
+    MATRIX  Matrix={0};                     // Matrix data for the GTE
+
     DoubleDB::InitGraph();
     InitController();
+    LoadDebugFont();
     
     while(1) {
-
+        ClearOTagR(DoubleDB::GetCurrentDB()->ot, OTLEN);  // Clear ordering table
         //INPUT - UPDATE
         GetInput(&x,&y,speed);
 
-        //DRAW
-        CreateTile(20, 30, 80, 60, col);
-        CreateTile(x, y, 50, 50, 255, 255, 255);
-        CreateTile(80, 150, 50, 50, 0, 255, 60);
-        CreateTile(250, 180, 50, 50, 40, 10, 250);
-        CreateTile(220, 80, 70, 50, 40, 70, 60);
-        CreateTile(180, 120, 50, 70, 40, 100, 0);
+        Rotate.vy = x; // Pan
+        Rotate.vx = y; // Tilt
 
+        //DRAW
+        //CreateTile(30, 50, 100, 80, col);
+        //CreateTriangle(x, y, 25, 25, 255, 255, 0, DoubleDB::GetCurrentDB());
+        //CreateTile(90, 150, 60, 50, 0, 255, 60);
+        CreateCube(DoubleDB::GetCurrentDB(), &Rotate, &Trans, &Scale, &Matrix);
+        // CreateTile(250, 180, 50, 50, 40, 10, 250);
+        // CreateTile(220, 80, 70, 50, 40, 70, 60);
+        // CreateTile(180, 120, 50, 70, 40, 100, 0);
+        PrintText("hello world",10,20);
         // Update the display
         DoubleDB::Blit();
     }
